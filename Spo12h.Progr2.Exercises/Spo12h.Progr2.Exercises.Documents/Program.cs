cd1c0b4261816e2bd5f2e27e3a609c1700e49e14
lefko <andreas.l@adb.se>
2013-11-13 18:16:02 +0100

    Added Documents Project

---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2.Exercises.Documents/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spo12h.Progr2.Exercises.Documents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
