fbff587ebec78fc4a1314bc32a48684a12d75bbc
lefko <andreas.l@adb.se>
2013-11-14 09:34:59 +0100

    Added Exercise 3A

---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2.Exercise3A/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spo12h.Progr2.Exercise3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int sum = 1;
+            Console.Write("\nEnter a number: ");
+            number = int.Parse(Console.ReadLine());
+            while (number <= 0)
+            {
+                Console.WriteLine("Enter a valid number");
+            }
+                
+            string partialOutput = "1";
+            string output;
+
+            for (int i = 2; i <= number; i++)
+            {
+                sum += i;
+                
+                partialOutput = partialOutput +  "+" + i ;
+            }
+
+            output = partialOutput + "=" + sum;
+
+            Console.WriteLine(output);
+
+            Console.ReadLine();
+        }
+    }
+}
