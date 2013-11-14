e8a6caa6eb71a6a9d119dc816722584b797c5e5f
lefko <andreas.l@adb.se>
2013-11-14 11:55:09 +0100

    Added Slask-Project

---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2.Slask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spo12h.Progr2.Slask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // TODO: 1. Skapa en lista med users
+            // TODO: 2. Lägg några users i listan
+            // TODO: 3. Skriv ut users på skärmen
+
+            var users = new List<User>();
+
+
+            users.Add(new User("Andreas", "Lefkopoulos", true));
+            users.Add(new User("Dag", "", true));
+            users.Add(new User("Anna", "", false));
+
+            foreach (User user in users)
+            {
+                Console.WriteLine(user.ToString());
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2.Slask/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spo12h.Progr2.Slask
+{
+    class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsActive { get; set; }
+        
+        public User(string  firstName, string lastName, bool isActive)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.IsActive = isActive;
+        }
+
+        public override string ToString()
+        {
+            string output;
+            output = string.Format("First Name: {0}\n" +
+                                   "LastName: {1}\n" +
+                                   "User Active: {2}", FirstName, LastName, IsActive);
+            return output;
+        }
+    }
+}
