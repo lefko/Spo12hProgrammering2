cbd3b489bd78ddb731ed0cf40a3759eaa6e6fcdb
lefko <andreas.l@adb.se>
2013-11-14 15:23:13 +0100

    Added Exercise 7 with quite some work done

---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2.Exercise7/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spo12h.Progr2.Exercise7
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SocialSecurityNumber { get; set; }
+
+        public Employee(string firstName, string lastName, string socSecNr)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.SocialSecurityNumber = socSecNr;
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}", FirstName, LastName);
+            }
+        }
+    }
+}
---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2.Exercise7/EmployeeCatalog.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spo12h.Progr2.Exercise7
+{
+    class EmployeeCatalog
+    {
+        List<Employee> employees = new List<Employee>();
+
+        public bool AddEmployee(string fName, string lName, string socSecNr)
+        {
+            // TODO: implement method
+            Employee foundEmployee = FindEmployee(socSecNr);
+
+            if (foundEmployee == null)
+            {
+                Employee employeeToAdd = new Employee(fName, lName, socSecNr);
+                employees.Add(employeeToAdd);
+                return true;
+            }
+            
+            return false;
+        }
+
+        public bool RemoveEmployee(string socSecNr)
+        {
+            Employee foundEmployee = FindEmployee(socSecNr);
+
+            if (foundEmployee != null)
+            {
+                    employees.Remove(foundEmployee);
+                return true;
+            }
+
+            return false;
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            // TODO: Is this what is to be returned?
+
+            return employees;
+        }
+
+        public Employee FindEmployee(string socSecNr)
+        {
+            Employee foundEmployee = null;
+
+            foreach (Employee employee in employees)
+            {
+                if (employee.SocialSecurityNumber == socSecNr)
+                    foundEmployee = employee;
+                break;
+            }
+
+            return foundEmployee;
+        }
+    }
+}
