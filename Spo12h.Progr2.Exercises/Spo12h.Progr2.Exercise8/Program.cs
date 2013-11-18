a2d39d32dbe3da00ac4e2da5c8393bbf56f5ceba
lefko <andreas.l@adb.se>
2013-11-18 13:12:43 +0100

    Added Exercise 8

---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2.Exercise8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spo12h.Progr2.Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
