02b593c967de3f4fbd994922b56dc1fb9cbb38f9
lefko <andreas.l@adb.se>
2013-11-19 17:27:12 +0100

    Added Testproject for Exercise 10

---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2.Exercise10.Tests/Classes.Tests/Logger.Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Spo12h.Progr2.Exercise10.Tests.Classes.Tests
+{
+    [TestFixture]
+    class Logger
+    {
+        [Test]
+        public void FirstTestMethod()
+        {
+            
+        }
+    }
+}
