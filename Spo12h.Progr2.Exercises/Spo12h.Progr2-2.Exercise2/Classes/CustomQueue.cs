397f369616895445d5cef9e593925ae3819d1952
lefko <andreas.l@adb.se>
2013-11-20 15:39:17 +0100

    Exerrcise 2B

---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2-2.Exercise2/Classes/CustomQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spo12h.Progr2_2.Exercise2.Classes
+{
+    public class CustomQueue<T>
+    {
+        public List<T> myQueue;
+
+        public CustomQueue()
+        {
+            myQueue = new List<T>();
+        }
+
+        public T Peak()
+        {
+            return myQueue.First();
+        }
+
+        public T Dequeue()
+        {
+            var myItem = myQueue.First();
+            myQueue.RemoveAt(0);
+
+            return myItem;
+        }
+
+        public void Enqueue(T item)
+        {
+            myQueue.Add(item);
+        }
+    }
+}
