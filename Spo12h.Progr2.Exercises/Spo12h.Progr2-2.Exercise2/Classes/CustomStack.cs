69c0c74521ae40b31935d311b44875054c062ae1
lefko <andreas.l@adb.se>
2013-11-20 14:33:15 +0100

    Exercise 2A

---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2-2.Exercise2/Classes/CustomStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spo12h.Progr2_2.Exercise2.Classes
+{
+    public class CustomStack<T>
+    {
+        public List<T> myList;
+
+        public CustomStack()
+        {
+            myList = new List<T>();
+        }
+    
+        public T Peek()
+        {
+            return myList.Last();
+
+        }
+
+        public T Pop()
+        {
+            var toReturn = myList.Last();
+            myList.Remove(myList.Last());
+
+            return toReturn;
+
+        }
+
+        public void Push(T item)
+        {
+            myList.Add(item);
+        }
+    }
+}
---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2-2.Exercise2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spo12h.Progr2_2.Exercise2.Classes;
+
+namespace Spo12h.Progr2_2.Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                CustomStack<int> intStack = new CustomStack<int>();
+                CustomStack<string> strStack = new CustomStack<string>();
+
+                intStack.Push(5);
+                intStack.Push(10);
+
+                strStack.Push("Hej");
+                strStack.Push("Laban");
+
+                Console.WriteLine("Peek the int Stack");
+                Console.WriteLine(intStack.Peek());
+
+                Console.WriteLine("Peek the string Stack");
+                Console.WriteLine(strStack.Peek());
+
+                Console.WriteLine("Int Pop");
+                var intPop = intStack.Pop();
+                Console.WriteLine(intPop);
+                Console.WriteLine("Peek the int Stack");
+                Console.WriteLine(intStack.Peek());
+
+                Console.WriteLine("String Pop");
+                var strPop = strStack.Pop();
+                Console.WriteLine(strPop);
+                Console.WriteLine("Peek the string Stack");
+                Console.WriteLine(strStack.Peek());
+            }
+            catch (Exception)
+            {
+                
+                throw new InvalidExpressionException();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
