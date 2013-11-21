1faa342284d4143ea8cbebd3cb17660b175b9ef0
lefko <andreas.l@adb.se>
2013-11-21 11:33:45 +0100

    Exercise 3 DONE

---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2-2.Exercise3/Classes/ListLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spo12h.Progr2_2.Exercise3.Classes
+{
+    class ListLoader
+    {
+        public List<string> languageList = new List<string>();
+ 
+        public void Loader()
+        {
+            StreamReader fileReader = new StreamReader("languages.txt");
+
+            string line;
+            while (!fileReader.EndOfStream/*(line = fileReader.ReadLine()) != null // This also works. To use it remove the line decl from the foreach loop*/)
+            {
+                line = fileReader.ReadLine();
+                languageList.Add(line); // Add to list.
+                // Console.WriteLine(line); // Write to console.
+            }
+
+            fileReader.ReadLine();
+
+        }
+    }
+}
--- Spo12h.Progr2.Exercises/Spo12h.Progr2-2.Exercise3/Program.cs
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2-2.Exercise3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Spo12h.Progr2_2.Exercise3.Classes;
 
 namespace Spo12h.Progr2_2.Exercise3
 {
@@ -10,6 +11,49 @@ namespace Spo12h.Progr2_2.Exercise3
     {
         static void Main(string[] args)
         {
+            ListLoader listLoader = new ListLoader();
+
+            listLoader.Loader();
+
+            DrawApp();
+            string searchTerm = GetInput();
+
+            IEnumerable<string> searchResult = ExecuteSearch(listLoader, searchTerm);
+           
+            PrintResult(searchResult);
+
+            Console.ReadLine();
+        }
+
+        static void DrawApp()
+        {
+            Console.WriteLine("\nEXERCISE 3");
+            Console.WriteLine("-----------\n");
+        }
+
+        static string GetInput()
+        {
+            Console.Write("Enter a search term: ");
+            string input = Console.ReadLine();
+
+            return input;
+        }
+
+        static IEnumerable<string> ExecuteSearch(ListLoader list, string searchTerm)
+        {
+            var searchResult = from l in list.languageList
+                                    where l.ToLower().Contains(searchTerm.ToLower())
+                                    select l;
+
+            return searchResult;
+        }
+
+        static void PrintResult(IEnumerable<string> results)
+        {
+            foreach (string result in results)
+            {
+                Console.WriteLine(result);
+            }
         }
     }
 }
