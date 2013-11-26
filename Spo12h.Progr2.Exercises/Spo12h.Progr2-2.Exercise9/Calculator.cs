35e0426da45597d82e99f8926afed60a2d294d41
lefko <andreas.l@adb.se>
2013-11-26 11:40:59 +0100

    Minor changes to Exercise 9

---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2-2.Exercise9/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spo12h.Progr2_2.Exercise9
+{
+    class Calculator
+    {
+
+        // Create a delegate called CalcDelegate that returns a double and takes
+        // two arguments. Both of these arguments should be of type double as well.
+        public object CalcLogic { get; set; }
+
+        public void PrintCalculation(int i, int i1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Spo12h.Progr2.Exercises/Spo12h.Progr2-2.Exercise9/Program.cs
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2-2.Exercise9/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,22 @@ namespace Spo12h.Progr2_2.Exercise9
     {
         static void Main(string[] args)
         {
+
+            // Create new calculator instance
+            var calc = new Calculator();
+            // Define the logic through a lambda expression
+            calc.CalcLogic = (a, b) => a + b;
+            // This should PRINT the number 15 to the console
+            calc.PrintCalculation(12, 3);
+
+            //Product
+            //calc.CalcLogic = (a, b) => a*b;
+
+
+            // Create a delegate called CalcDelegate that returns a double and takes
+            // two arguments. Both of these arguments should be of type double as well.
+
         }
+        
     }
 }
