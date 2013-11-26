10122b886d505d5f8eddae8fb9c85a31297b0778
lefko <andreas.l@adb.se>
2013-11-26 10:06:26 +0100

    Added Exercise 9 Project

---
+++ Spo12h.Progr2.Exercises/Spo12h.Progr2-2.Exercise9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spo12h.Progr2_2.Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
